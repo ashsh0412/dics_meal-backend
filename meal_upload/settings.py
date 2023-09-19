f5d943db3d8dddad2faa3dd378356472deda4ef9
daveg7lee <daveg7lee@gmail.com>
2023-09-19 22:35:48 +0900

    🐛 settings 파일 수정

--- meal_upload/settings.py
+++ meal_upload/settings.py
@@ -26,13 +26,10 @@ SECRET_KEY = os.environ.get(
     'SECRET_KEY', default="django-insecure-%9m8uet4f=69)v8&@o$u1mf3t8lhaehb6gscbk1&qdgmnsbtw(")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
